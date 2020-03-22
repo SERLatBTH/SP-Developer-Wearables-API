891f075aa816e220bf2ac53803daedf5b27ad543
Ludwig Johnson <31315278+ludwig-johnson@users.noreply.github.com>
2020-03-22 17:03:52 +0100

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "Only tested with a fresh install of Ubuntu Server 18.04. No guarantees are made. Use Ctrl+C to exit or any key to continue"
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
